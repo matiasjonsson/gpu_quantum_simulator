cd96833abeea167db72293fba87fe9fa3de2e842
Matias G Jonsson <mjonsson@unix5.andrew.cmu.edu>
2021-04-27 19:00:28 -0400

    Further small changes. Added some comments if you want to look at what I've done.

--- simulator.cpp
+++ simulator.cpp
@@ -14,14 +14,12 @@ struct sparse_elt {
     complex<double> val;
 };
 
-float normSquared(complex<double> amplitude) {
-    return real(amplitude * conj(amplitude));
-}
+
 
 bool isNormalized (vector<complex<double> > state) {
     double sum = 0;
     for (auto i = state.begin(); i != state.end(); ++i) {
-        sum += normSquared(*i);
+        sum += norm(*i);
     }
     return (abs(1.0 - sum) < 1e-15);
 }
@@ -74,16 +72,25 @@ vector<sparse_elt> tensor(vector<sparse_elt> u1, vector<sparse_elt> u2) {
 
 
 vector<sparse_elt> hadamard() {
-    vector<sparse_elt> hadamard;
+    vector<sparse_elt> had;
     // Dimensions
-    hadamard.push_back(createElt(2, 2, 0.0));
+    had.push_back(createElt(2, 2, 0.0));
     // Values
     double temp = 1.0 / sqrt(2);
-    hadamard.push_back(createElt(0, 0, temp));
-    hadamard.push_back(createElt(0, 1, temp));
-    hadamard.push_back(createElt(1, 0, temp));
-    hadamard.push_back(createElt(1, 1, -temp));
-    return hadamard;
+    had.push_back(createElt(0, 0, temp));
+    had.push_back(createElt(0, 1, temp));
+    had.push_back(createElt(1, 0, temp));
+    had.push_back(createElt(1, 1, -temp));
+    return had;
+}
+vector<sparse_elt> identity(){
+    vector<sparse_elt> id;
+    // Dimensions
+    id.push_back(createElt(2, 2, 0.0));
+    // Values
+    id.push_back(createElt(0, 0, 1.0));
+    id.push_back(createElt(1, 1, 1.0));
+    return id;
 }
 
 vector<sparse_elt> hadamardAll(const int n) {
@@ -94,13 +101,31 @@ vector<sparse_elt> hadamardAll(const int n) {
     }
     return hadAll;
 }
+vector<sparse_elt> hadamardOne(const int n, const int which) {
+    vector<sparse_elt> had = hadamard();
+    vector<sparse_elt> id = identity();
+    vector<sparse_elt> total = (which == 0 ? had : id);
+    for(int i = 1; i < n; i++) {
+        if(which == i) 
+            total = tensor(total, had);
+        else
+            total = tensor(total, id);
+    }
+    return total;
+}
 
 void printComplex(complex<double> amplitude) {
     cout << "(" << real(amplitude) << "+" << imag(amplitude) << "i)";
 }
-void printVec(vector<complex<double> > state, const int n, const int N) {
+bool equalsZero(complex<double> amplitude) {
+    return (norm(amplitude) < 1e-15);
+}
+void printVec(vector<complex<double> > state, const int n, const int N, bool printAll) {
     cout << "State:\n";
     for (int i = 0; i < N; ++i) {
+        if (!printAll && equalsZero(state.at(i)))
+            continue;
+
         printComplex(state.at(i));
         cout << "|";
         for (int j = n-1; j >= 0; --j) {
@@ -110,21 +135,35 @@ void printVec(vector<complex<double> > state, const int n, const int N) {
         cout << "\n";
     }
 }
-void printVecProbs(vector<complex<double> > state, const int n, const int N) {
+void printVecProbs(vector<complex<double> > state, const int n, const int N, bool printAll) {
     cout << "Probability distribution:\n";
     for (int i = 0; i < N; ++i) {
+        if (!printAll && equalsZero(state.at(i)))
+            continue;
+
         cout << "|<";
         for (int j = n-1; j >= 0; --j) {
             cout << (0x1 & (i>>j));
         }
-        cout << "|Psi>|^2 = " << normSquared(state.at(i)) << "\n";
+        cout << "|Psi>|^2 = " << norm(state.at(i)) << "\n";
     }
 }
 
 int main() {
-    const int n = 7;
+    const int n = 3;
     const int N = pow(2, n);
-    vector<complex<double> > state = classical(N, 0);
-    state = applyOperator(state, hadamardAll(n));
-    printVec(state, n, N);
+    vector<complex<double> > state = classical(N, 7);
+    //Two ways of applying hadamard to all qubits:
+    //Method 1: much faster, do each hadamard independently.
+    for (int i = 0; i < n; ++i) {
+        state = applyOperator(state, hadamardOne(n, i));    
+    }
+
+    //Method 2: much slower due to not being sparse, creates
+    //one single dense matrix that hadamards all the qubits at once.
+    //state = applyOperator(state, hadamardAll(n));
+    
+    //Only print if the number of qubits is small, else will be very long.
+    printVec(state, n, N, false);
+    //printVecProbs(state, n, N, false);
 }
