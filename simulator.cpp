b7024dca6bf004fe70496fe7b07337cfb8d2e828
Matias G Jonsson <mjonsson@unix6.andrew.cmu.edu>
2021-05-03 20:35:06 -0400

    finished making CNOT gate and other features such as printing out gates. Also made CU1 (controlled 1 qubit gate) that is more general than CNOT.

--- simulator.cpp
+++ simulator.cpp
@@ -9,12 +9,22 @@ using namespace std;
 // Sparse matrix reserves the first element for dimension, assuming matrix is square.
 // It is implemented using a vector.
 struct sparse_elt {
-    int x;
-    int y;
-    complex<double> val;
+    int u;
+    int v;
+    complex<double> amp;
 };
 
+sparse_elt createElt(int u, int v, complex<double> amp) {
+    sparse_elt elt;
+    elt.u = u;
+    elt.v = v;
+    elt.amp = amp;
+    return elt;
+}
 
+bool equalsZero(complex<double> amplitude) {
+    return (norm(amplitude) < 1e-15);
+}
 
 bool isNormalized (vector<complex<double> > state) {
     double sum = 0;
@@ -41,91 +51,37 @@ vector<complex<double> > classical(const int N, const int init) {
 }
 
 vector<complex<double> > applyOperator(vector<complex<double> > state, vector<sparse_elt> unitary) {
-    assert(unitary.begin().x == state.size());
+    assert(unitary.begin()->u == state.size());
     vector<complex<double> > newstate = zero(state.size());
     for(auto i = unitary.begin() + 1; i != unitary.end(); ++i) {
-        newstate.data()[i->x] += i->val * state.data()[i->y];
+        newstate.data()[i->u] += i->amp * state.data()[i->v];
     }
     return newstate;
 }
 
-sparse_elt createElt(int x, int y, complex<double> val) {
-    sparse_elt elt;
-    elt.x = x;
-    elt.y = y;
-    elt.val = val;
-    return elt;
-}
-
 vector<sparse_elt> tensor(vector<sparse_elt> u1, vector<sparse_elt> u2) {
-    int dim1 = u1.begin()->x;
-    int dim2 = u2.begin()->x;
+    int dim1 = u1.begin()->u;
+    int dim2 = u2.begin()->u;
     vector<sparse_elt> u3;
     u3.push_back(createElt(dim1*dim2, dim1*dim2, 0.0));
     for(auto i = u1.begin() + 1; i != u1.end(); ++i) {
         for(auto j = u2.begin() + 1; j != u2.end(); ++j) {
-            u3.push_back(createElt(i->x * dim2 + j->x, i->y * dim2 + j->y, i->val * j->val));
+            u3.push_back(createElt(i->u * dim2 + j->u, i->v * dim2 + j->v, i->amp * j->amp));
         }
     }
     return u3;
 }
 
-
-vector<sparse_elt> hadamard() {
-    vector<sparse_elt> had;
-    // Dimensions
-    had.push_back(createElt(2, 2, 0.0));
-    // Values
-    double temp = 1.0 / sqrt(2);
-    had.push_back(createElt(0, 0, temp));
-    had.push_back(createElt(0, 1, temp));
-    had.push_back(createElt(1, 0, temp));
-    had.push_back(createElt(1, 1, -temp));
-    return had;
-}
-vector<sparse_elt> identity(){
-    vector<sparse_elt> id;
-    // Dimensions
-    id.push_back(createElt(2, 2, 0.0));
-    // Values
-    id.push_back(createElt(0, 0, 1.0));
-    id.push_back(createElt(1, 1, 1.0));
-    return id;
-}
-
-vector<sparse_elt> hadamardAll(const int n) {
-    vector<sparse_elt> had1 = hadamard();
-    vector<sparse_elt> hadAll = had1;
-    for(int i = 1; i < n; i++) {
-        hadAll = tensor(had1, hadAll);
-    }
-    return hadAll;
-}
-vector<sparse_elt> hadamardOne(const int n, const int which) {
-    vector<sparse_elt> had = hadamard();
-    vector<sparse_elt> id = identity();
-    vector<sparse_elt> total = (which == 0 ? had : id);
-    for(int i = 1; i < n; i++) {
-        if(which == i) 
-            total = tensor(total, had);
-        else
-            total = tensor(total, id);
-    }
-    return total;
-}
+/* Print features */
 
 void printComplex(complex<double> amplitude) {
     cout << "(" << real(amplitude) << "+" << imag(amplitude) << "i)";
-}
-bool equalsZero(complex<double> amplitude) {
-    return (norm(amplitude) < 1e-15);
 }
 void printVec(vector<complex<double> > state, const int n, const int N, bool printAll) {
     cout << "State:\n";
     for (int i = 0; i < N; ++i) {
         if (!printAll && equalsZero(state.at(i)))
             continue;
-
         printComplex(state.at(i));
         cout << "|";
         for (int j = n-1; j >= 0; --j) {
@@ -148,22 +104,123 @@ void printVecProbs(vector<complex<double> > state, const int n, const int N, boo
         cout << "|Psi>|^2 = " << norm(state.at(i)) << "\n";
     }
 }
+void printOp(vector<sparse_elt> U) {
+    vector<vector<complex<double> > > densified;
+    int dim = U.begin()->u;
+    densified.assign(dim, zero(dim));
+    for (auto x = U.begin()+1; x != U.end(); x++) {
+        densified.data()[x->u].data()[x->v] += x->amp;
+    }
+    for (int x = 0; x < dim; x++) {
+        for (int y = 0; y < dim; y++) {
+            cout << densified.data()[x].data()[y] << " ";
+        }
+        cout << "\n";
+    }
+}
+
+/* One qubit gates */ 
+vector<sparse_elt> identity(){
+    vector<sparse_elt> id;
+    // dimensions
+    id.push_back(createElt(2, 2, 0.0));
+    // values
+    id.push_back(createElt(0, 0, 1.0));
+    id.push_back(createElt(1, 1, 1.0));
+    return id;
+}
+
+vector<sparse_elt> naught() {
+    vector<sparse_elt> naught;
+    // dimensions
+    naught.push_back(createElt(2, 2, 0.0));
+    // values
+    naught.push_back(createElt(1, 0, 1.0));
+    naught.push_back(createElt(0, 1, 1.0));
+    return naught;
+}
+vector<sparse_elt> hadamard() {
+    vector<sparse_elt> had;
+    // dimensions
+    had.push_back(createElt(2, 2, 0.0));
+    // values
+    double norm = 1.0 / sqrt(2);
+    had.push_back(createElt(0, 0, norm));
+    had.push_back(createElt(0, 1, norm));
+    had.push_back(createElt(1, 0, norm));
+    had.push_back(createElt(1, 1, -norm));
+    return had;
+}
+
+
+/* Expanding operations for 1 qubit gates. */
+
+/* It is much more efficient to do many 1 qubit gates sequentially than to compute their tensor product */
+vector<sparse_elt> oneQubitGateExpand(vector<sparse_elt> oneQubitGate, const int n, const int which) {
+    vector<sparse_elt> total = (which == 0 ? oneQubitGate : identity());
+    for(int i = 1; i < n; i++) {
+        if(which == i) 
+            total = tensor(total, oneQubitGate);
+        else
+            total = tensor(total, identity());
+    }
+    return total;
+}
+
+
+/* Two qubit gates */
+vector<sparse_elt> CNOTExpanded(const int n, const int u, const int v) { 
+    vector<sparse_elt> cnot;
+    int N = pow(2, n);
+    cnot.push_back(createElt(N, N, 0.0));
+    int j;
+    for(int i = 0; i < N; i++) {
+        if (i>>(n-u-1) & 0x1) {
+            j = i ^ (0x1<<(n-v-1));
+            cnot.push_back(createElt(i, j, 1.0));
+        } else {
+            cnot.push_back(createElt(i, i, 1.0));
+        }
+    }
+    return cnot;
+}
+vector<sparse_elt> CU1Expanded(vector<sparse_elt> U1, const int n, const int u, const int v) {
+    vector<sparse_elt> CU1;
+    int N = pow(2, n);
+    CU1.push_back(createElt(N, N, 0.0));
+    vector<complex<double> > stateOfV;
+    for(long i = 0; i < N; i++) {
+        if (i>>(n-u-1) & 0x1) {
+            stateOfV = classical(2, (i>>(n-v-1)) & 0x1);
+            stateOfV = applyOperator(stateOfV, U1);
+            if(!equalsZero(stateOfV.data()[0])) {
+                CU1.push_back(createElt(i, i & ~(0x1L<<(n-v-1)), stateOfV.data()[0]));
+            }
+            if(!equalsZero(stateOfV.data()[1])) {
+                CU1.push_back(createElt(i, i | (0x1L<<(n-v-1)), stateOfV.data()[1]));
+            }
+        } else {
+            CU1.push_back(createElt(i, i, 1.0));
+        }
+    }
+    return CU1;
+}
+
+
 
 int main() {
-    const int n = 3;
+    const int n = 2;
     const int N = pow(2, n);
-    vector<complex<double> > state = classical(N, 7);
-    //Two ways of applying hadamard to all qubits:
-    //Method 1: much faster, do each hadamard independently.
-    for (int i = 0; i < n; ++i) {
-        state = applyOperator(state, hadamardOne(n, i));    
-    }
-
-    //Method 2: much slower due to not being sparse, creates
-    //one single dense matrix that hadamards all the qubits at once.
-    //state = applyOperator(state, hadamardAll(n));
+    vector<complex<double> > state = classical(N, 0);
     
-    //Only print if the number of qubits is small, else will be very long.
+    /* Uniform superposition: */
+    /*for (int i = 0; i < n; ++i) {
+        state = applyOperator(state, oneQubitGateExpand(hadamard(), n, i));    
+    }*/
+    state = applyOperator(state, oneQubitGateExpand(naught(), n, 0));
+    state = applyOperator(state, oneQubitGateExpand(naught(), n, 1));
+    state = applyOperator(state, CU1Expanded(hadamard(), n, 0, 1));
+
     printVec(state, n, N, false);
     //printVecProbs(state, n, N, false);
 }
